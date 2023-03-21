dd31ba454511b97c2a0fdb591c5fe9e059dc3f29
bigmide <olumideojo99@gmail.com>
2023-03-21 13:03:29 +0100

    Implementation fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '40da8884-1fc5-4023-a591-cbe3e2c782a1';
+export const KEY = '7b6260d6-fcee-4a71-aecc-0a4ea95cedd7';
 export const MODAL_CLOSE_SEC = 2.5;
